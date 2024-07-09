91d7ce340a50060e280260b23dd87195283bb982
teddy <ted@gmail.com>
2024-07-09 15:18:46 +0000

    form validation

---
+++ FormValidation/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using FormValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormValidation.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        public IActionResult WeaklyTypedLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginPost(string username, string password)
+        {
+            ViewBag.Username = username;
+            ViewBag.Password = password;
+            return View();  
+        }
+        public IActionResult StronglyTypedLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginSuccess(LoginViewModel Login)
+        {
+            ViewBag.username = Login.Username;
+            ViewBag.password = Login.Password;
+            return View();
+        }
+    }
+}
---
+++ FormValidation/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace FormValidation.Models
+{
+    public class LoginViewModel
+    {
+        public string ? Username { get; set; }
+
+        public string ? Password { get; set; }
+
+
+    }
+}
